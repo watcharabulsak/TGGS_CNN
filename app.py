e34d47be2fefbc255458e838607a2a1269453936
watcharabulsak <thiraprarom@gmail.com>
2021-04-16 14:51:11 +0700

    second commit

--- app.py
+++ app.py
@@ -25,38 +25,87 @@ from firebase_admin import credentials
 
 
 
-app = Flask(__name__, template_folder= 'template')
+app = Flask(__name__, template_folder= 'public')
 
-UPLOAD_FOLDER = '/Users/thiraprarom/TGGS/Project/upload_folder'
+# UPLOAD_FOLDER = '/Users/thiraprarom/TGGS/Project/upload_folder'
+# UPLOAD_FOLDER = 'gs://visaulab.appspot.com'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/uploadfile', methods=['GET', 'POST'])
-def upload_file():
-    # print('Test')
+# @app.route('/uploadfile', methods=['GET', 'POST'])
+# def upload_file():
+#     # print('Test')
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             print('no file')
+#             return redirect(url_for('index'))
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             print('no filename')
+#             return redirect(url_for('index'))
+#         else:
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#       #send file name as parameter to downlad
+#             print("saved file successfully")
+#             print(filename)
+#             return render_template("index.html", result = filename)
+#             # return redirect('/return-files/'+ filename)
+
+
+
+config = {
+    #Hidden
+    apiKey: "AIzaSyAJsdM1eZr0JYT1sFNfuCtLhIKK6N21Hm0",
+    authDomain: "visaulab.firebaseapp.com",
+    projectId: "visaulab",
+    storageBucket: "visaulab.appspot.com",
+    messagingSenderId: "650456939169",
+    appId: "1:650456939169:web:83964f3d4d4192f89f8ee0"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+auth = firebase.auth()
+
+@app.route('/', methods=['GET', 'POST'])
+#Login
+def basic():
+    unsuccessful = 'Please check your credentials'
+    successful = 'Login successful'
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('no file')
-            return redirect(url_for('index'))
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('no filename')
-            return redirect(url_for('index'))
-        else:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      #send file name as parameter to downlad
-            print("saved file successfully")
-            print(filename)
-            return render_template("index.html", result = filename)
-            # return redirect('/return-files/'+ filename)
+        email = request.form['name']
+        password = request.form['pass']
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            return render_template('index.html', s=successful)
+        except:
+            return render_template('index.html', us=unsuccessful)
+    return render_template('index.html')
+
+#Posting function
+@app.route('/uploadfile', methods=['GET','POST'])
+def uploadfile():
+    if request.method == 'POST':
+        try:
+            path_on_cloud = "images/newproduct.jpg"
+            path_local=request.form['file']
+            storage.child(path_on_cloud).put(path_local)
+            return render_template("index.html")
+        except:
+            return render_template("index.html")
+
+
+
+
+
 
 @app.route("/predict")
 def predictimage():
