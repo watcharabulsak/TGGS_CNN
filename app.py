71f0ff133b25517b1db1277ac1561887250ac019
watcharabulsak <thiraprarom@gmail.com>
2021-04-17 11:24:11 +0700

    "change uploadfile route"

--- app.py
+++ app.py
@@ -40,28 +40,30 @@ UPLOAD_FOLDER = firebase.app().storage('gs://visaulab.appspot.com')
 def index():
     return render_template('index.html')
 
-# @app.route('/uploadfile', methods=['GET', 'POST'])
-# def upload_file():
-#     # print('Test')
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             print('no file')
-#             return redirect(url_for('index'))
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit a empty part without filename
-#         if file.filename == '':
-#             print('no filename')
-#             return redirect(url_for('index'))
-#         else:
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#       #send file name as parameter to downlad
-#             print("saved file successfully")
-#             print(filename)
-#             return render_template("index.html", result = filename)
-#             # return redirect('/return-files/'+ filename)
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+    # print('Test')
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(url_for('index'))
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            return redirect(url_for('index'))
+        else:
+            file = request.files['file']
+            firebase.storage().put(file)
+            filename = secure_filename(file.filename)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      #send file name as parameter to downlad
+            print("saved file successfully")
+            print(filename)
+            return render_template("index.html", result = filename)
+            # return redirect('/return-files/'+ filename)
 
 
 
@@ -97,25 +99,25 @@ if __name__ == "__main__":
     app.run(debug= True)
  
 
-@app.route('/messenger/message/send/picture/individual', methods=['post'])
-def send_individual_picture():
-    picture = request.files['picture']
-    temp = tempfile.namedtemporaryfile(delete= False)
-    picture.save(temp.name)
-    firebase.storage().put(temp.name)
-    # clean-up temp image
-    os.remove(temp.name) 
+# @app.route('/messenger/message/send/picture/individual', methods=['post'])
+# def send_individual_picture():
+#     picture = request.files['picture']
+#     temp = tempfile.namedtemporaryfile(delete= False)
+#     picture.save(temp.name)
+#     firebase.storage().put(temp.name)
+#     # clean-up temp image
+#     os.remove(temp.name) 
     
     
-@app.route('/messenger/message/send/picture/individual', methods=['post'])
-def send_individual_picture():
-    picture = request.files['picture']
-    firebase.storage().put(picture)
+# @app.route('/messenger/message/send/picture/individual', methods=['post'])
+# def send_individual_picture():
+#     picture = request.files['picture']
+#     firebase.storage().put(picture)
 
-@app.route('/messenger/message/send/picture/individual', methods=['post'])
-def send_individual_picture():
-    picture = request.files['picture']
-    firebase.storage().put(picture) 
+# @app.route('/messenger/message/send/picture/individual', methods=['post'])
+# def send_individual_picture():
+#     picture = request.files['picture']
+#     firebase.storage().put(picture) 
 
 
 # @app.route('/', methods=['GET', 'POST'])
